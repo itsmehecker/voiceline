3a547dfaef82a19c19cc076b7a7637abbcf1e557
Vaishnav Sandeep <90813931+itsmehecker@users.noreply.github.com>
2024-12-23 05:48:20 +0000

    remodel

---
+++ public/client.js
@@ -0,0 +1,52 @@
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let localStream;
+let peerConnection;
+const socket = new WebSocket('ws://localhost:8080');
+
+const constraints = {
+  audio: true,
+  video: false
+};
+
+startButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia(constraints);
+  peerConnection = new RTCPeerConnection();
+  localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+  peerConnection.onicecandidate = ({ candidate }) => {
+    if (candidate) {
+      socket.send(JSON.stringify({ type: 'candidate', candidate }));
+    }
+  };
+
+  peerConnection.ontrack = ({ streams: [stream] }) => {
+    const audio = document.createElement('audio');
+    audio.srcObject = stream;
+    audio.play();
+  };
+
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  socket.send(JSON.stringify({ type: 'offer', offer }));
+};
+
+stopButton.onclick = () => {
+  localStream.getTracks().forEach(track => track.stop());
+  peerConnection.close();
+  socket.close();
+};
+
+socket.onmessage = async ({ data }) => {
+  const message = JSON.parse(data);
+  if (message.type === 'offer') {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.send(JSON.stringify({ type: 'answer', answer }));
+  } else if (message.type === 'answer') {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(message.answer));
+  } else if (message.type === 'candidate') {
+    await peerConnection.addIceCandidate(new RTCIceCandidate(message.candidate));
+  }
+};
